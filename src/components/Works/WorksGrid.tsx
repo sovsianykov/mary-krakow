e2e7308774ae55870620394ec7b8f81f74de42e8
“sovsianykov” <swebproger@gmail.com>
2023-03-11 21:09:54 +0100

    variant3

--- src/components/Works/WorksGrid.tsx
+++ src/components/Works/WorksGrid.tsx
@@ -1,16 +1,18 @@
-import React, {memo} from 'react';
-import {Grid} from "@mui/material";
-import {images} from "@/assets/mock";
+import React, { FC, memo } from "react";
+import { Grid } from "@mui/material";
+import { images } from "@/assets/mock";
 import WorksItem from "@/components/Works/WorksItem";
 
-const WorksGrid = () => {
-    return (
-        <Grid container spacing={2} mt={2} style={{width:"100%"}} >
-            {images.map(el =><Grid item xs={12} sm={6} md={4} key={el.id}>
-                <WorksItem image={el}/>
-            </Grid>)}
+const WorksGrid: FC = () => {
+  return (
+    <Grid container spacing={2} mt={2} style={{ width: "100%" }}>
+      {images.map((el) => (
+        <Grid item xs={12} sm={6} md={4} key={el.id}>
+          <WorksItem image={el} />
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
 
 export default memo(WorksGrid);
