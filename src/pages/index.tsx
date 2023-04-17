253a7e93016a5340b2dd1c095d6f6cf2209d09cd
sovsianykov <swebproger@gmail.com>
2023-04-17 12:29:13 +0200

    final commit with getStatic Props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@
 import Layout from "@/components/Layout/Layout";
 import HomePage from "@/components/Home/HomePage";
 
-
+export async function getStaticProps() {
+    return {
+        props: {},
+    }
+}
 
 
 export default function Home() {
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import Layout from "@/components/Layout/Layout";
 import WorksGrid from "@/components/Works/WorksGrid";
 
+export async function getStaticProps() {
+    return {
+        props: {},
+    }
+}
+
 const Works = () => {
     return (
         <Layout pageTitle="Art Works">
